8c58d780a6fce2987ddafee34761af0ef4507fe1
Harshit Sharma <55287441+code-harsh-123@users.noreply.github.com>
2021-05-11 13:22:19 +0530

    frequency code added

--- code.cpp
+++ code.cpp
@@ -72,5 +72,45 @@ int main() {
 //T.C = O(n)
 //S.C = O(n)
 
+/* Frequency of the elements of an array */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,m,max=0;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	max=a[0];
+	//cout<<max<<endl;
+	for(int i=1;i<n;i++)
+	{
+		if(max<a[i])
+		{
+			max=a[i];
+		}													
+	}								//a[5] = 1 2 1 3 4   
+	m=max+1;							//b[5] = 0 1 2 3 4 	
+	//cout<<m<<endl;								   
+	int b[m];
+	for(int i=0;i<m;i++)
+	{
+		b[i]=0;
+	}
+	for(int i=0;i<n;i++)
+	{
+		b[a[i]] += 1;
+	}
+	for(int i=0;i<m;i++)
+	{
+		cout<<b[i]<<" ";
+	}
+	return 0;
+}
+
 
 // Thankyou guys
