2bbc5b8df0d4a6403d24177f31e5e9d917bedd49
Harshit Sharma <55287441+code-harsh-123@users.noreply.github.com>
2021-05-11 13:07:28 +0530

    Coding_Template Done

---
+++ code.cpp
@@ -0,0 +1,32 @@
+// Welcome to this coding template 
+
+/* Reverse the elements of array */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,cnt=0;
+        cin>>n;
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        reverse(a,a+n);
+        for(int i=0;i<n;i++)
+        {
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+
+//T.C = O(n)
+//S.C = O(n)
